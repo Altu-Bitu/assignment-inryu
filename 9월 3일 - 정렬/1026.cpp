aee75adcb7daf34c78bd5a93a730c84d6744f294
Inryu <dellintel17@naver.com>
2021-09-03 23:52:19 +0900

    [정렬] 9월 3일 - Update

--- 9월 3일 - 정렬/1026.cpp
+++ 9월 3일 - 정렬/1026.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int main(){
-    int N,val;
-    vector<int> A,B;
+int main() {
+    int N, val;
+    vector<int> A, B;
 
-    cin>>N;
+    cin >> N;
 
-    for(int i=0;i<N;i++){
-        cin>>val;
+    for (int i = 0; i < N; i++) {
+        cin >> val;
         A.push_back(val);
     }
-    for(int i=0;i<N;i++){
-        cin>>val;
+    for (int i = 0; i < N; i++) {
+        cin >> val;
         B.push_back(val);
     }
 
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end(),greater<>()); //근데 B를 재배열해도 되나..
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<>()); //근데 B를 재배열해도 되나..
 
-    int res=0;
-    for(int i=0;i<N;i++){
-        res+=A[i]*B[i];
+    int res = 0;
+    for (int i = 0; i < N; i++) {
+        res += A[i] * B[i];
     }
-    cout<<res<<"\n";
+    cout << res << "\n";
     return 0;
 }
--- 9월 3일 - 정렬/1316.cpp
+++ 9월 3일 - 정렬/1316.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 int N;
-int main(){
-    cin>>N;
-    int cnt=0;
+
+int main() {
+    cin >> N;
+    int cnt = 0;
     string str;
-    while(N){
-        vector<bool> visited(26,0); //알파벳 개수만큼.
-        cin>>str;
+    while (N--) {
+        vector<bool> visited(26, 0); //알파벳 개수만큼.
+        cin >> str;
         cnt++;
-        visited[str.at(0)-'a']=true; //맨 첫 캐릭터 체크 // -'a'를 이용해 0인덱스부터 들어가도록.
-        for(int i=1;i<str.size();i++){
-            if(str.at(i)==str.at(i-1)) continue; //이전 캐릭터와 같다면(연속) continue
-            if(visited[str.at(i)-'a']) { //연속해 있지 않고, 이미 방문한 캐릭터
+        visited[str.at(0) - 'a'] = true; //맨 첫 캐릭터 체크 // -'a'를 이용해 0인덱스부터 들어가도록.
+        for (int i = 1; i < str.size(); i++) {
+            if (str.at(i) == str.at(i - 1)) continue; //이전 캐릭터와 같다면(연속) continue
+            if (visited[str.at(i) - 'a']) { //연속해 있지 않고, 이미 방문한 캐릭터
                 cnt--;
                 break;
             }
-            visited[str.at(i)-'a']=true; //처음 본 캐릭터 방문 체크
+            visited[str.at(i) - 'a'] = true; //처음 본 캐릭터 방문 체크
         }
-        N--;
     }
-    cout<<cnt<<"\n";
+    cout << cnt << "\n";
     return 0;
 }
